18b3dfa2d27bcf828af8ec011a76331d3ff83574
Julien Simonet <julien.simonet@tohero.fr>
2025-06-20 15:14:10 +0200

    deploy api

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -10,8 +10,8 @@ import { NewsModule } from '../news/news.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
